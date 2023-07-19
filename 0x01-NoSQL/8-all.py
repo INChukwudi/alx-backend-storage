3840bdabfe5ebacc91426f851ed773962ad0e922
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-07-18 19:38:51 -0700

    added task 8 solution file, 8-all.py

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+File containing the list_all function
+"""
+
+
+def list_all(mongo_collection):
+    """
+    Function that lists all documents in a collection
+
+    Args:
+    mongo_collection - collection from a mongoDb
+
+    Return:
+    value - list bearing the collection documents
+    """
+    documents = list(mongo_collection.find({}))
+    return documents
