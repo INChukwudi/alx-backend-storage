90c6a4c20ac0da0582fa3cbc0dda9cb2f5e45c3a
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-07-18 21:57:24 -0700

    fixed styling issues in 101-students.py

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -15,7 +15,7 @@ def top_students(mongo_collection):
     for doc in students_docs:
         topics = doc["topics"]
 
-        total_score = sum(topic['score'] for topic in topics)
+        total_score = sum(topic["score"] for topic in topics)
         average_score = total_score / len(topics)
 
         student_info = {
@@ -27,6 +27,6 @@ def top_students(mongo_collection):
         students_info.append(student_info)
 
     students_info = sorted(
-      students_info, key=lambda x: x['averageScore'], reverse=True)
+      students_info, key=lambda x: x["averageScore"], reverse=True)
 
     return students_info
