f459b1b82b23e8c64d8f86a60996b1d565b79310
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-07-18 21:55:22 -0700

    added task 14 (advanced task) solution file, 101-students.py

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""
+File containing top_students function
+"""
+
+
+def top_students(mongo_collection):
+    """
+    Function that returns all students sorted by average score
+    """
+    students_docs = mongo_collection.find()
+    students_info = []
+
+    for doc in students_docs:
+        topics = doc["topics"]
+
+        total_score = sum(topic['score'] for topic in topics)
+        average_score = total_score / len(topics)
+
+        student_info = {
+            "_id": doc["_id"],
+            "name": doc["name"],
+            "averageScore": average_score
+        }
+
+        students_info.append(student_info)
+
+    students_info = sorted(
+      students_info, key=lambda x: x['averageScore'], reverse=True)
+
+    return students_info
